8f10252048023e68a85cc2a91bcc8eb015543e1d
StuartIanNaylor <StuartIanNaylor@outlook.com>
2019-04-11 03:41:56 +0100

    zephemeral boot added

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ install -m 644 ztab /etc/ztab
 mkdir -p /usr/local/share/zram-config
 mkdir -p /usr/local/share/zram-config/log
 install -m 644 uninstall.sh /usr/local/share/zram-config/uninstall.sh
+install -m 644 ro-root.sh /usr/local/share/zram-config/ro-root.sh
 install -m 644 zram-config.logrotate /etc/logrotate.d/zram-config
 mkdir -p /usr/local/lib/zram-config/
 install -m 755 overlayfs-tools/overlay /usr/local/lib/zram-config/overlay
