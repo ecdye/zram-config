90f2bbfc84583673cf2c2b141d51fc7af2032c14
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-07-24 18:54:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
 
-systemctl -q is-active zram-config  && { echo "ERROR: zram-config service is still running. Please run \"sudo service zram-config stop\" to stop it and uninstall"; exit 1; }
+systemctl -q is-active zram-config  && { echo "ERROR: zram-config service is still running. Please run \"sudo service zram-config stop\" to stop it and u$
 [ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
 [ -d /usr/local/bin/zram-config ] && { echo "zram-config is already installed, uninstall first"; exit 1; }
 
-InstallXattr () {
-apt-get install libattr1-dev
-git clone https://github.com/kmxz/overlayfs-tools
+
+apt-get -y install libattr1-dev
+
+git clone https://github.com/StuartIanNaylor/overlayfs-tools
 cd overlayfs-tools
 make
 cd ..
 
-}
 
-InstallAttr () {
-apt-get install libattr1-dev
-git clone -b fix_xattr_lib_include https://github.com/Izual750/overlayfs-tools
-cd overlayfs-tools
-make
-cd ..
-}
-
-
-if ldconfig -p | grep libattr
-then
-        InstallAttr
-else
-        InstallXattr
-fi
-
-
-#BRANCH=next rpi-update
-
-# zram-config install 
+# zram-config install
 install -m 755 zram-config /usr/local/bin/
 install -m 644 zram-config.service /etc/systemd/system/zram-config.service
 install -m 644 ztab /etc/ztab
@@ -42,10 +23,9 @@ install -m 644 uninstall.sh /usr/local/share/zram-config/uninstall.sh
 install -m 644 ro-root.sh /usr/local/share/zram-config/ro-root.sh
 install -m 644 zram-config.logrotate /etc/logrotate.d/zram-config
 mkdir -p /usr/local/lib/zram-config/
+
 install -m 755 overlayfs-tools/overlay /usr/local/lib/zram-config/overlay
 systemctl enable zram-config
 
 echo "#####          Reboot to activate zram-config         #####"
 echo "#####       edit /etc/ztab to configure options       #####"
-
-
