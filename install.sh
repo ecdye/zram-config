231442f486876c0f0e97d729777a00ab21b1215e
StuartIanNaylor <stuartiannaylor@outlook.com>
2019-07-25 19:28:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-
-git clone https://github.com/StuartIanNaylor/overlayfs-tools -b Arch
+systemctl -q is-active zram-config  && { echo "ERROR: zram-config service is still running. Please run \"sudo service zram-config stop\" to stop it and u$
+[ "$(id -u)" -eq 0 ] || { echo "You need to be ROOT (sudo can be used)"; exit 1; }
+[ -d /usr/local/bin/zram-config ] && { echo "zram-config is already installed, uninstall first"; exit 1; }
+apt-get -y install libattr1-dev
+git clone https://github.com/StuartIanNaylor/overlayfs-tools
 cd overlayfs-tools
 make
 cd ..
-
-
 # zram-config install
 install -m 755 zram-config /usr/local/bin/
 install -m 644 zram-config.service /etc/systemd/system/zram-config.service
@@ -17,9 +18,7 @@ install -m 644 uninstall.sh /usr/local/share/zram-config/uninstall.sh
 install -m 644 ro-root.sh /usr/local/share/zram-config/ro-root.sh
 install -m 644 zram-config.logrotate /etc/logrotate.d/zram-config
 mkdir -p /usr/local/lib/zram-config/
-
 install -m 755 overlayfs-tools/overlay /usr/local/lib/zram-config/overlay
 systemctl enable zram-config
-
 echo "#####          Reboot to activate zram-config         #####"
 echo "#####       edit /etc/ztab to configure options       #####"
