6de2b5ba2c1596d5d6dbdd85129d955fb1bf87f9
Ethan Dye <mrtops03@gmail.com>
2022-01-20 15:44:24 -0700

    Refine expect script
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- tests/install-packages.bash
+++ tests/install-packages.bash
@@ -3,10 +3,10 @@
 export DEBIAN_FRONTEND="noninteractive"
 
 apt-get --quiet update
-# apt-get --quiet upgrade --yes
+apt-get --quiet upgrade --yes
 apt-get --quiet install --yes gcc make libc6-dev
 apt-get --quiet autoremove --yes
-systemctl mask systemd-logind.service
+systemctl mask rpi-eeprom-update.service hciuart.service
 rm -f /var/lib/apt/lists/lock
 rm -f /var/cache/apt/archives/lock
 rm -f /var/lib/dpkg/lock*
