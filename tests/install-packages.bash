b28f9971cfe8d75c44abd860bf26f380c398404e
Ethan Dye <mrtops03@gmail.com>
2022-01-21 16:33:55 -0700

    Refine testing
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- tests/install-packages.bash
+++ tests/install-packages.bash
@@ -6,7 +6,7 @@ apt-get --quiet update
 apt-get --quiet upgrade --yes
 apt-get --quiet install --yes gcc make libc6-dev
 apt-get --quiet autoremove --yes
-systemctl mask rpi-eeprom-update.service hciuart.service
+systemctl mask rpi-eeprom-update.service hciuart.service systemd-logind.service
 rm -f /var/lib/apt/lists/lock
 rm -f /var/cache/apt/archives/lock
 rm -f /var/lib/dpkg/lock*
--- tests/setup-image.bash
+++ tests/setup-image.bash
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-if ! [[ -f $1 ]]; then
+if ! [[ -f $2 ]]; then
   curl -s -L "https://downloads.raspberrypi.org/raspios_lite_armhf_latest" -o "$1"
+  curl -s "$(curl "https://downloads.raspberrypi.org/raspios_lite_armhf_latest" -s -L -I  -o /dev/null -w '%{url_effective}')".sig -o "${1}.sig"
+  gpg -q --keyserver keyserver.ubuntu.com --recv-key 0x8738CD6B956F460C
+  gpg -q --trust-model always --verify "${1}.sig" "$1"
+  unzip -q "$1" -d .
+  qemu-img resize -f raw "$2" 4G
+  echo ", +" | sfdisk -N 2 "$2"
+  loopPrefix="$(kpartx -asv "$2" | grep -oE "loop([0-9]+)" | head -n 1)"
+  mkdir -p tests/{fs,kernel,dtb}
+  mount -o rw -t ext4 "/dev/mapper/${loopPrefix}p2" "tests/fs"
+  mount -o rw -t vfat "/dev/mapper/${loopPrefix}p1" "tests/fs/boot"
+  cp tests/fs/boot/kernel* tests/kernel
+  cp tests/fs/boot/*.dtb tests/dtb
+  rsync -avr --exclude="*.zip" --exclude="*.img" --exclude="tests/fs" --exclude="tests/dtb" --exclude="tests/kernel" ./ tests/fs/opt/zram
+  systemd-nspawn --directory="tests/fs" /opt/zram/tests/install-packages.bash
+  umount tests/fs/boot
+  umount tests/fs
+  kpartx -d "$2"
 fi
-curl -s "$(curl "https://downloads.raspberrypi.org/raspios_lite_armhf_latest" -s -L -I  -o /dev/null -w '%{url_effective}')".sig -o "${1}.sig"
-gpg -q --keyserver keyserver.ubuntu.com --recv-key 0x8738CD6B956F460C
-gpg -q --trust-model always --verify "${1}.sig" "$1"
-unzip -q "$1" -d .
-mv *-raspios-*.img raspios.img
-qemu-img resize -f raw raspios.img 4G
-echo ", +" | sfdisk -N 2 raspios.img
-loopPrefix="$(kpartx -asv raspios.img | grep -oE "loop([0-9]+)" | head -n 1)"
-mkdir -p tests/{fs,kernel,dtb}
-mount -o rw -t ext4 "/dev/mapper/${loopPrefix}p2" "tests/fs"
-mount -o rw -t vfat "/dev/mapper/${loopPrefix}p1" "tests/fs/boot"
-cp tests/fs/boot/kernel* tests/kernel
-cp tests/fs/boot/*.dtb tests/dtb
-rsync -avr --exclude="*.zip" --exclude="*.img" --exclude="tests/fs" --exclude="tests/dtb" --exclude="tests/kernel" ./ tests/fs/opt/zram
-systemd-nspawn --directory="tests/fs" /opt/zram/tests/install-packages.bash
-umount tests/fs/boot
-umount tests/fs
-kpartx -d raspios.img
 exit 0
