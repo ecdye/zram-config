dc4ea9ec3f06d1cc83a1da03532f22fc1e554467
Ethan Dye <mrtops03@gmail.com>
2022-01-22 15:39:24 -0700

    Add the missing file
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

---
+++ tests/test-zram-devices.bash
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+check_zram_mounts() {
+  while read -r line; do
+    case "$line" in
+      "#"*)
+        # Skip comment line
+        continue
+        ;;
+
+      "")
+        # Skip empty line
+        continue
+        ;;
+
+      *)
+        # shellcheck disable=SC2086
+        set -- $line
+        ZTYPE="$1"
+	      TARGET_DIR="$5"
+        if [[ $ZTYPE == "swap" ]] && [[ "$(swapon | grep -q zram)" ]]; then
+          echo "Test failed: swap not on zram."
+          return 1
+        else
+          if [[ "$(df "$TARGET_DIR" | awk '/overlay/ { print $1 }' | tr -d '0-9')" != "overlay" ]]; then
+            echo "Test failed: overlay for '$TARGET_DIR' not found." >&3
+            return 1
+          fi
+        fi
+        ;;
+    esac
+  done < /etc/ztab
+}
+
+check_zram_removal() {
+  while read -r line; do
+    case "$line" in
+      "#"*)
+        # Skip comment line
+        continue
+        ;;
+
+      "")
+        # Skip empty line
+        continue
+        ;;
+
+      *)
+        # shellcheck disable=SC2086
+        set -- $line
+        ZTYPE="$1"
+	      TARGET_DIR="$5"
+        if [[ $ZTYPE == "swap" ]] && ! [[ "$(swapon | grep -q zram)" ]]; then
+          echo "Test failed: swap still on zram."
+          return 1
+        else
+          if ! [[ "$(df "$TARGET_DIR" | awk '/overlay/ { print $1 }' | tr -d '0-9')" != "overlay" ]]; then
+            echo "Test failed: overlay for '$TARGET_DIR' found."
+            return 1
+          fi
+        fi
+        ;;
+    esac
+  done < /etc/ztab
+}
+
+if [[ $1 == "removal" ]]; then
+  check_zram_removal || exit 1
+else
+  check_zram_mounts || exit 1
+fi
