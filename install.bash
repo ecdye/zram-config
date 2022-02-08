10c52402cbfd5616f033d4b157d5d50817fe76fa
Jonathan Vermeulen <jmavermeulen@gmail.com>
2022-02-08 03:53:39 +0100

    Add alpine support
    Signed-off-by: Jonathan Vermeulen <jmavermeulen@gmail.com>

--- install.bash
+++ install.bash
@@ -1,32 +1,42 @@
 #!/usr/bin/env bash
 
 BASEDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+OS="$(grep -o '^ID=.*$' /etc/os-release | cut -d'=' -f2)"
 
 if [[ "$(id -u)" -ne 0 ]]; then
   echo "ERROR: You need to be ROOT (sudo can be used)."
   exit 1
 fi
 if [[ $1 == "sync" ]]; then
-  install -m 644 "$BASEDIR"/zsync.* /etc/systemd/system/
-  systemctl daemon-reload
-  systemctl enable --now zsync.timer
+  if [[ "$OS" == "alpine" ]]; then
+    install -m 755 "$BASEDIR"/zsync.cron /etc/periodic/daily/zsync
+  else
+    install -m 644 "$BASEDIR"/zsync.timer /etc/systemd/system/
+    install -m 644 "$BASEDIR"/zsync.service /etc/systemd/system/
+    systemctl daemon-reload
+    systemctl enable --now zsync.timer
+  fi
   echo "#####     zsync service is now installed     #####"
   exit 0
 fi
-if [[ $(systemctl is-active zram-config.service) == "active" ]]; then
+if [[ "$(systemctl is-active zram-config.service 2> /dev/null)" == "active" ]] || rc-service zram-config status; then
   echo -e "ERROR: zram-config service is still running.\\nPlease run \"sudo ${BASEDIR}/update.bash\" to update zram-config instead."
   exit 1
 fi
-if [[ -f /usr/local/sbin/zram-config ]]; then
+if [[ -f /usr/local/sbin/zram-config ]] || [[ -f /usr/sbin/zram-config ]]; then
   echo -e "ERROR: zram-config is already installed.\\nPlease run \"sudo ${BASEDIR}/update.bash\" to update zram-config instead."
   exit 1
 fi
 
-if ! dpkg -s 'gcc' 'make' 'libc6-dev' &> /dev/null; then
+if [[ $OS == "alpine" ]] && ! [[ "$(apk info 2> /dev/null | grep -E '^(gcc|make|fts-dev|linux-headers|util-linux-misc|musl-dev)' | tr '\n' ' ')" == "fts-dev gcc make util-linux-misc musl-dev linux-headers " ]]; then
+  echo "Installing needed packages (gcc, make, fts-dev, linux-headers, util-linux-misc, musl-dev)"
+  apk add gcc make fts-dev linux-headers util-linux-misc musl-dev || exit 1
+elif ! dpkg -s 'gcc' 'make' 'libc6-dev' &> /dev/null; then
   echo "Installing needed packages (gcc, make, libc6-dev)"
   apt-get install --yes gcc make libc6-dev || exit 1
 fi
-if [[ "$(grep -o '^ID=.*$' /etc/os-release | cut -d'=' -f2)" == "ubuntu" ]] && [[ $(bc -l <<< "$(grep -o '^VERSION_ID=.*$' /etc/os-release | cut -d'=' -f2 | tr -d '"') >= 21.10") -eq 1 ]]; then
+
+if [[ $OS == "ubuntu" ]] && [[ $(bc -l <<< "$(grep -o '^VERSION_ID=.*$' /etc/os-release | cut -d'=' -f2 | tr -d '"') >= 21.10") -eq 1 ]]; then
   echo "Install zram module package for Ubuntu (linux-modules-extra-raspi)"
   if ! dpkg -s 'linux-modules-extra-raspi' &> /dev/null; then
     apt-get install --yes linux-modules-extra-raspi || exit 1
@@ -36,8 +46,14 @@ fi
 make --always-make --directory="${BASEDIR}/overlayfs-tools"
 
 echo "Installing zram-config files"
-install -m 755 "${BASEDIR}/zram-config" /usr/local/sbin/
-install -m 644 "${BASEDIR}/zram-config.service" /etc/systemd/system/zram-config.service
+if [[ "$OS" == "alpine" ]]; then
+  install -m 755 "${BASEDIR}/zram-config" /usr/sbin/
+  install -m 755 "${BASEDIR}/zram-config.openrc" /etc/init.d/zram-config
+else
+  install -m 755 "${BASEDIR}/zram-config" /usr/local/sbin/
+  install -m 644 "${BASEDIR}/zram-config.service" /etc/systemd/system/zram-config.service
+  echo "ReadWritePaths=/usr/local/share/zram-config/log" >> /lib/systemd/system/logrotate.service
+fi
 install -m 644 "${BASEDIR}/ztab" /etc/ztab
 mkdir -p /usr/local/share/zram-config/log
 ln -s /usr/local/share/zram-config/log /var/log/zram-config
@@ -45,14 +61,18 @@ install -m 755 "${BASEDIR}/uninstall.bash" /usr/local/share/zram-config/uninstal
 install -m 644 "${BASEDIR}/zram-config.logrotate" /etc/logrotate.d/zram-config
 mkdir -p /usr/local/lib/zram-config/
 install -m 755 "${BASEDIR}/overlayfs-tools/overlay" /usr/local/lib/zram-config/overlay
-echo "ReadWritePaths=/usr/local/share/zram-config/log" >> /lib/systemd/system/logrotate.service
-
-echo "Starting zram-config.service"
-systemctl daemon-reload
-systemctl enable --now zram-config.service
-until [[ $(systemctl show -p SubState --value zram-config) == "exited" ]]; do
-  sleep 5
-done
+if [[ "$OS" == "alpine" ]]; then
+  echo "Starting zram-config service..."
+  rc-update add zram-config boot
+  rc-service zram-config start
+else
+  echo "Starting zram-config.service"
+  systemctl daemon-reload
+  systemctl enable --now zram-config.service
+  until [[ $(systemctl show -p SubState --value zram-config) == "exited" ]]; do
+    sleep 5
+  done
+fi
 
 echo "#####     zram-config is now installed and running     #####"
 echo "#####       edit /etc/ztab to configure options        #####"
--- uninstall.bash
+++ uninstall.bash
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
 
 BASEDIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+OS="$(grep -o '^ID=.*$' /etc/os-release | cut -d'=' -f2)"
 
 if [[ "$(id -u)" -ne 0 ]]; then
   echo "ERROR: You need to be ROOT (sudo can be used)."
   exit 1
 fi
-if ! [[ -f /usr/local/sbin/zram-config ]]; then
+if ! [[ -f /usr/local/sbin/zram-config ]] || ! [[ -f /usr/sbin/zram-config ]]; then
   echo "ERROR: zram-config is not installed."
   exit 1
 fi
 
 zram-config "stop"
 tar -cf "${BASEDIR}/logs.tar" --directory=/usr/local/share/zram-config log
-systemctl disable zram-config.service
-rm -f /etc/systemd/system/zram-config.service
-if [[ -f /etc/systemd/system/zsync.timer ]]; then
-  systemctl disable zsync.timer
-  rm -f /etc/systemd/system/zsync.*
+if [[ $OS == "alpine" ]]; then
+  rc-service zram-config stop
+  rc-update del zram-config boot
+  if [[ -f /etc/periodic/daily/zsync ]]; then
+    rm -f /etc/periodic/daily/zsync
+  fi
+  rm -f /etc/init.d/zram-config
+  rm -f /usr/sbin/zram-config
+else
+  systemctl disable zram-config.service
+  rm -f /etc/systemd/system/zram-config.service
+  if [[ -f /etc/systemd/system/zsync.timer ]]; then
+    systemctl disable zsync.timer
+    rm -f /etc/systemd/system/zsync.*
+  fi
+  sed -i '\|^ReadWritePaths=/usr/local/share/zram-config/log$|d' /lib/systemd/system/logrotate.service
+  systemctl daemon-reload
+  rm -f /usr/local/sbin/zram-config
 fi
-sed -i '\|^ReadWritePaths=/usr/local/share/zram-config/log$|d' /lib/systemd/system/logrotate.service
-systemctl daemon-reload
-rm -f /usr/local/sbin/zram-config
 rm -f /etc/logrotate.d/zram-config
 rm -f /etc/ztab
 rm -rf /usr/local/lib/zram-config
