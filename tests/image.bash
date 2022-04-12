02f05f2363ad04befbcb5d7be5f5fbfa07c3642f
Ethan Dye <mrtops03@gmail.com>
2022-04-11 22:59:03 -0600

    Fix tests broken by upstream changes (#90)
    Raspberry Pi OS recently switched to a new image compression method of
    XZ. This resolves the incompatibility with the new form of compression.
    
    Additionally, Raspberry Pi OS no longer ships with a default username
    and password and as such we now need to create a username and password
    manually in order to run our tests.
    
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- tests/image.bash
+++ tests/image.bash
@@ -10,6 +10,7 @@ imageFile() {
     mount -o rw -t ext4 "/dev/mapper/${loopPrefix}p2" "tests/fs"
     mount -o rw -t vfat "/dev/mapper/${loopPrefix}p1" "tests/fs/boot"
   elif [[ $1 == "umount" ]]; then
+    sync
     umount tests/fs/boot
     umount tests/fs
     kpartx -d "$2"
@@ -22,12 +23,12 @@ if [[ $1 == "setup" ]]; then
     curl -s "$(curl "https://downloads.raspberrypi.org/raspios_lite_armhf_latest" -s -L -I  -o /dev/null -w '%{url_effective}')".sig -o "${2}.sig"
     gpg -q --keyserver keyserver.ubuntu.com --recv-key 0x8738CD6B956F460C
     gpg -q --trust-model always --verify "${2}.sig" "$2"
-    unzip -q "$2" -d .
+    xz "$2" -d
   fi
   qemu-img resize -f raw "$3" 4G
   echo ", +" | sfdisk -N 2 "$3"
   imageFile "mount" "$3"
-  rsync -avr --exclude="*.zip" --exclude="*.img" --exclude="*.sig" --exclude="tests/fs" --exclude="tests/dtb" --exclude="tests/kernel" ./ tests/fs/opt/zram
+  rsync -avr --exclude="*.img" --exclude="*.sig" --exclude="tests/fs" --exclude="tests/dtb" --exclude="tests/kernel" ./ tests/fs/opt/zram
   systemd-nspawn --directory="tests/fs" /opt/zram/tests/install-packages.bash
   echo "set enable-bracketed-paste off" >> tests/fs/etc/inputrc  # Prevents weird character output
   cp tests/fs/boot/kernel* tests/kernel
