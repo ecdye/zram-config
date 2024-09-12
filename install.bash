53814b26bc69f2eb5b86643d0aaf4b6d0c5f7037
nan mu <mu.nan.11@outlook.com>
2024-09-12 14:38:22 +0800

    Fix #119
    Signed-off-by: nan mu <mu.nan.11@outlook.com>

--- install.bash
+++ install.bash
@@ -38,7 +38,12 @@ elif ! dpkg -s 'gcc' 'meson' 'libc6-dev' &> /dev/null; then
 fi
 
 UBUNTU_VERSION="$(grep -o '^VERSION_ID=.*$' /etc/os-release | cut -d'=' -f2 | tr -d '"')"
-if [[ $OS == "ubuntu" ]] && [[ $(bc -l <<< "$UBUNTU_VERSION >= 21.10") -eq 1 ]]; then
+if [[ $OS == "ubuntu" ]] && [[ $(bc -l <<< "$UBUNTU_VERSION >= 23.10") -eq 1 ]]; then
+  echo "Installing zram module package for Ubuntu (linux-modules)"
+  if ! dpkg -s linux-modules-$(uname -a | awk '{print $3}') &> /dev/null; then
+    apt-get install --yes dpkg -s linux-modules-$(uname -a | awk '{print $3}') || exit 1
+  fi
+elif [[ $OS == "ubuntu" ]] && [[ $(bc -l <<< "$UBUNTU_VERSION >= 21.10") -eq 1 ]]; then
   echo "Installing zram module package for Ubuntu (linux-modules-extra-raspi)"
   if ! dpkg -s 'linux-modules-extra-raspi' &> /dev/null; then
     apt-get install --yes linux-modules-extra-raspi || exit 1
