0b8359395ecdfa74e096b58d59bb2a92fdef79a9
Ethan Dye <mrtops03@gmail.com>
2024-12-17 22:50:32 -0700

    Ensure that files sync to disk on SystemD (#123)
    This ensures that zram-config has the correct dependency ordering to
    startup and shutdown correctly. In this case, making it start up as
    early as possible so we are ready before anyone starts writing to our
    directories. This includes starting up before rsyslog and
    systemd-journald.
    
    An added conflict with shutdown targets is required to make sure that
    the service stops before shutdown when DefaultDependencies is disabled.
    
    IgnoreOnIsolate simply prevents taking the service down when isolating
    other units.
    
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- tests/test-zram-devices.bash
+++ tests/test-zram-devices.bash
@@ -19,7 +19,7 @@ check_zram_mounts() {
         # shellcheck disable=SC2086
         set -- $line
         ZTYPE="$1"
-	      TARGET_DIR="$5"
+        TARGET_DIR="$5"
         if [[ $ZTYPE == "swap" ]]; then
           if [[ "$(swapon | awk '/zram/ { print $1 }' | tr -d '0-9')" != "/dev/zram" ]]; then
             echo "Test failed: swap not on zram."
@@ -55,7 +55,7 @@ check_zram_removal() {
         # shellcheck disable=SC2086
         set -- $line
         ZTYPE="$1"
-	      TARGET_DIR="$5"
+        TARGET_DIR="$5"
         if [[ $ZTYPE == "swap" ]]; then
           if [[ "$(swapon | awk '/zram/ { print $1 }' | tr -d '0-9')" == "/dev/zram" ]]; then
             echo "Test failed: swap on zram."
@@ -76,6 +76,7 @@ check_zram_removal() {
 
 if [[ $1 == "removal" ]]; then
   check_zram_removal || exit 1
+  test -f /var/log/test || echo "Test failed: /var/log/test not found."
 else
   check_zram_mounts || exit 1
 fi
