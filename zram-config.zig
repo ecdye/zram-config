7fd42d76775064866be6a9e9f8d8bb87e1ebfc5c
Ethan Dye <mrtops03@gmail.com>
2025-04-18 00:09:20 +0000

    Improve error handling and configuration
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- zram-config.zig
+++ zram-config.zig
@@ -5,13 +5,13 @@ const SYS = linux.SYS;
 const Allocator = std.mem.Allocator;
 const log = std.log;
 
-fn load_zram_mod(alloc: Allocator) !bool {
+fn load_zram_mod(alloc: Allocator) !?i8 {
     const maybe_dir = std.fs.openDirAbsolute("/sys/module/zram", .{}) catch null;
     if (maybe_dir) |dir_const| {
         var dir = dir_const;
         dir.close();
         log.debug("zram already loaded", .{});
-        return true;
+        return null;
     }
 
     const Utsname = extern struct {
@@ -28,13 +28,12 @@ fn load_zram_mod(alloc: Allocator) !bool {
     const res = linux.E.init(linux.syscall1(SYS.uname, @intFromPtr(&uts)));
     if (res != .SUCCESS) {
         log.err("failed to get uname: {s}", .{@tagName(res)});
-        return false;
+        return error.uname;
     }
 
     const release = std.mem.sliceTo(&uts.release, 0);
-    var buf: [std.fs.max_path_bytes]u8 = undefined;
-    const zram_module_path = try std.fmt.bufPrint(
-        buf[0..],
+    const zram_module_path = try std.fmt.allocPrint(
+        alloc,
         "/lib/modules/{s}/kernel/drivers/block/zram/zram.ko.zst",
         .{release},
     );
@@ -57,11 +56,11 @@ fn load_zram_mod(alloc: Allocator) !bool {
     ));
     if (sys_result != .SUCCESS) {
         log.err("init_module failed: {s}", .{@tagName(sys_result)});
-        return false;
+        return error.init_module;
     }
 
     log.debug("zram loaded successfully!", .{});
-    return true;
+    return 0;
 }
 
 fn init_zram_dev(
@@ -69,32 +68,31 @@ fn init_zram_dev(
     alg: []const u8,
     disk_s: []const u8,
     mem_l: []const u8,
-) i8 {
-    const dev_num = add_z_dev(alloc) catch |err| {
+    dev: ?i8,
+) !i8 {
+    const dev_n = dev orelse add_z_dev(alloc) catch |err| {
         log.err("failed to get new zram device: {!}", .{err});
-        return -1;
+        return err;
     };
-    const dev_config_path = std.fmt.allocPrint(alloc, "/sys/block/zram{d}", .{dev_num}) catch |err| {
-        log.err("failed to alloc dev config path: {!}", .{err});
-        return -1;
-    };
+    errdefer rem_z_dev(alloc, dev_n);
 
-    set_config_val(alloc, dev_config_path, "comp_algorithm", alg) catch |err| {
+    set_config_val(alloc, dev_n, "comp_algorithm", alg) catch |err| {
         log.err("failed to set compression alg: {!}", .{err});
-        return -1;
+        return err;
     };
 
-    set_config_val(alloc, dev_config_path, "disksize", disk_s) catch |err| {
+    set_config_val(alloc, dev_n, "disksize", disk_s) catch |err| {
         log.err("failed to set disk size: {!}", .{err});
-        return -1;
+        return err;
     };
 
-    set_config_val(alloc, dev_config_path, "mem_limit", mem_l) catch |err| {
+    set_config_val(alloc, dev_n, "mem_limit", mem_l) catch |err| {
         log.err("failed to set mem limit: {!}", .{err});
-        return -1;
+        return err;
     };
+    if (std.mem.eql(u8, mem_l, "0")) log.info("no memory limit set for /dev/zram{d}", .{dev_n});
 
-    return dev_num;
+    return dev_n;
 }
 
 fn add_z_dev(alloc: Allocator) !i8 {
@@ -110,23 +108,44 @@ fn add_z_dev(alloc: Allocator) !i8 {
 
 fn set_config_val(
     alloc: Allocator,
-    dev_config_path: []const u8,
+    dev_n: i8,
     setting: []const u8,
     val: []const u8,
 ) !void {
-    const config_path = try std.fmt.allocPrint(alloc, "{s}/{s}", .{ dev_config_path, setting });
+    const config_path = try std.fmt.allocPrint(alloc, "/sys/block/zram{d}/{s}", .{ dev_n, setting });
     const config_f = try std.fs.openFileAbsolute(config_path, .{ .mode = .write_only });
     defer config_f.close();
     _ = try config_f.write(val);
+}
+
+fn rem_z_dev(alloc: Allocator, dev_n: i8) void {
+    set_config_val(alloc, dev_n, "reset", "1") catch |err| {
+        log.err("failed to reset /dev/zram{d}: {!}", .{ dev_n, err });
+    };
+
+    const file = std.fs.openFileAbsolute(
+        "/sys/class/zram-control/hot_remove",
+        .{ .mode = .write_only },
+    ) catch |err| {
+        log.err("failed to get hot remove: {!}", .{err});
+        return;
+    };
+    defer file.close();
+    const dev_ns = std.fmt.allocPrint(alloc, "{d}", .{dev_n}) catch |err| {
+        log.debug("failed to alloc string of dev_n: {d}, error: {!}", .{ dev_n, err });
+        return;
+    };
+    _ = file.write(dev_ns) catch |err| {
+        log.err("failed to write to hot remove: {!}", .{err});
+    };
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
 
-    if (!try load_zram_mod(alloc)) return;
+    const init_num = try load_zram_mod(alloc);
 
-    const dev_num = init_zram_dev(alloc, "zstd", "2048", "1024");
-    if (dev_num == -1) return;
+    const dev_num = init_zram_dev(alloc, "zstd", "2048", "1024", init_num) catch return;
     log.info("configured dev num: {d}", .{dev_num});
 }
