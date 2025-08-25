b5f6721180d2a133e34e17d6d3b470deb3be18b5
Ethan Dye <mrtops03@gmail.com>
2025-08-25 13:21:03 -0600

    Cleanup on error
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- src/zram_config_lib/helpers.zig
+++ src/zram_config_lib/helpers.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const linux = std.os.linux;
 
 pub fn zblock_config_write(
     alloc: Allocator,
@@ -26,4 +27,51 @@ pub fn make_uuid_v4() [16]u8 {
     uuid[8] = (uuid[8] & 0x3f) | 0x80;
 
     return uuid;
+}
+
+pub fn mount_or_error(
+    special: ?[*:0]const u8,
+    dir: [*:0]const u8,
+    fstype: ?[*:0]const u8,
+    flags: u32,
+    data: usize,
+) !void {
+    const res = linux.E.init(linux.mount(special, dir, fstype, flags, data));
+    if (res != .SUCCESS) {
+        switch (res) {
+            .ACCES => return error.ACCES,
+            .BUSY => return error.BUSY,
+            .FAULT => return error.FAULT,
+            .INVAL => return error.INVAL,
+            .LOOP => return error.LOOP,
+            .MFILE => return error.MFILE,
+            .NAMETOOLONG => return error.NAMETOOLONG,
+            .NODEV => return error.NODEV,
+            .NOENT => return error.NOENT,
+            .NOMEM => return error.NOMEM,
+            .NOTBLK => return error.NOTBLK,
+            .NOTDIR => return error.NOTDIR,
+            .NXIO => return error.NXIO,
+            .PERM => return error.PERM,
+            .ROFS => return error.ROFS,
+            else => unreachable,
+        }
+    }
+}
+
+pub fn umount_or_error(special: [*:0]const u8, flags: u32) !void {
+    const res = linux.E.init(linux.umount2(special, flags));
+    if (res != .SUCCESS) {
+        switch (res) {
+            .AGAIN => return error.AGAIN,
+            .BUSY => return error.BUSY,
+            .FAULT => return error.FAULT,
+            .INVAL => return error.INVAL,
+            .NAMETOOLONG => return error.NAMETOOLONG,
+            .NOENT => return error.NOENT,
+            .NOMEM => return error.NOMEM,
+            .PERM => return error.PERM,
+            else => unreachable,
+        }
+    }
 }
