aae944910153315c35873c05cdee4707a93b3fcf
Ethan Dye <mrtops03@gmail.com>
2025-09-09 00:17:34 -0600

    Test installing zig in GH Actions
    Signed-off-by: Ethan Dye <mrtops03@gmail.com>

--- tests/install-packages.bash
+++ tests/install-packages.bash
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 export DEBIAN_FRONTEND="noninteractive"
+export NONINTERACTIVE=1
 
 apt-get --quiet update
-apt-get --quiet install --yes gcc meson libc6-dev
+apt-get --quiet install --yes build-essential procps curl file git
 apt-get --quiet autoremove --yes
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/home/linuxbrew/.linuxbrew/bin/brew install zig
 systemctl mask rpi-eeprom-update.service hciuart.service systemd-logind.service
 rm -f /var/lib/apt/lists/lock
 rm -f /var/cache/apt/archives/lock
